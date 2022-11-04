fb17aa667934f370ad6bc533cfac5611a07167d7
bishal-github <shreesbishal44@gmail.com>
2022-11-04 20:43:27 +0545

    adding routing elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-   
+   <Router>
+    <Routes>
+      <Route />
+    </Routes>
+   </Router>
   );
 }
 
