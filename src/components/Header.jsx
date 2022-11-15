23e28c4d51299af8a2fd371d3847de70b5d617c0
bishal-github <shreesbishal44@gmail.com>
2022-11-15 19:01:03 +0545

    adding links to the header jsx & adding css properties

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <h1>TechStar.</h1>
+        <main>
+            <Link to={"/"}>Home</Link>
+            <Link to={"/contact"}>Contact</Link>
+            <Link to={"/#about"}>About</Link>
+            <Link to={"/#brands"}>Brands</Link>
+            <Link to={"/services"}>Services</Link>
+        </main>
+    </nav>
   )
 }
 
