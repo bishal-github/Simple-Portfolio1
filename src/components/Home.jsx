176aae6afb050c02c99eb769198319a66e939814
bishal-github <shreesbishal44@gmail.com>
2022-11-16 15:09:46 +0545

    creating divs and adding images inside div and styling them

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import vg from '../assests/2.webp';
 
 const Home = () => {
   return (
@@ -9,6 +10,16 @@ const Home = () => {
         <p>Solution to all your problems</p>
       </main>
     </div>
+    <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+    <div>
+        <p>
+            We are your one and only solution to the tech problems you face everyday. We are leading tech company whose aim is to increase the problem solving ability in children.
+        </p>
+    </div>
+    </div>
+
     </>
   )
 }
