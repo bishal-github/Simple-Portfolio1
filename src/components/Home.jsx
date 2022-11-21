eafd93a0f13d4317d81c2d79465b58e9e78433ef
bishal-github <shreesbishal44@gmail.com>
2022-11-21 14:49:39 +0545

    adding content to home3 div and creating new home4 div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import vg from '../assests/2.webp';
+import {
+AiFillGoogleCircle,
+AiFillAmazonCircle,
+AiFillYoutube,
+AiFillInstagram
+} from 'react-icons/ai'
 
 const Home = () => {
   return (
     <>
     <div className='home'>
       <main>
-        <h1>TechyStar</h1>
+        <h1>React Developer</h1>
         <p>Solution to all your problems</p>
       </main>
     </div>
@@ -19,7 +25,22 @@ const Home = () => {
         </p>
     </div>
     </div>
+<div className="home3">
+    <div>
+        <h1>Who we are?</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque dolor tenetur necessitatibus! Exercitationem enim delectus quaerat explicabo magni architecto veniam ut alias quis, nostrum praesentium, natus ipsa vero commodi! Alias velit quia eveniet architecto itaque harum? Dolorum cumque, repudiandae consectetur qui illo illum iste numquam tempore sint consequatur soluta dignissimos excepturi fugiat quam maxime perferendis minus ipsa voluptatem ea rerum?</p>
+    </div>
+</div>
+<div className="home4">
+    <div>
+        <h1>Brands</h1>
+        <article>
+            <div>
 
+            </div>
+        </article>
+    </div>
+</div>
     </>
   )
 }
