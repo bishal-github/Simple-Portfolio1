7a93511ebdabf66bafd9fad9f898033000e752ba
bishal-github <shreesbishal44@gmail.com>
2022-11-21 19:24:29 +0545

    adding animation to the home4 div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ AiFillInstagram
 const Home = () => {
   return (
     <>
-    <div className='home'>
+    <div className='home' id='home'>
       <main>
         <h1>React Developer</h1>
         <p>Solution to all your problems</p>
@@ -25,13 +25,13 @@ const Home = () => {
         </p>
     </div>
     </div>
-<div className="home3">
+<div className="home3" id='about'>
     <div>
         <h1>Who we are?</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque dolor tenetur necessitatibus! Exercitationem enim delectus quaerat explicabo magni architecto veniam ut alias quis, nostrum praesentium, natus ipsa vero commodi! Alias velit quia eveniet architecto itaque harum? Dolorum cumque, repudiandae consectetur qui illo illum iste numquam tempore sint consequatur soluta dignissimos excepturi fugiat quam maxime perferendis minus ipsa voluptatem ea rerum?</p>
     </div>
 </div>
-<div className="home4">
+<div className="home4" id='brands'>
     <div>
         <h1>Brands</h1>
         <article>
@@ -39,15 +39,15 @@ const Home = () => {
                 <AiFillGoogleCircle />
                 <p>Google</p>
             </div>
-            <div style={{animationDelay:'0.3s'}}>
+            <div style={{animationDelay:'0.5s'}}>
                 <AiFillAmazonCircle />
                 <p>Amazon</p>
             </div>
-            <div style={{animationDelay:'0.3s'}}>
+            <div style={{animationDelay:'0.7s'}}>
                 <AiFillYoutube />
                 <p>Youtube</p>
             </div>
-            <div style={{animationDelay:'0.3s'}}>
+            <div style={{animationDelay:'1s'}}>
                 <AiFillInstagram />
                 <p>Instagram</p>
             </div>
