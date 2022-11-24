750c3b418364cf59e1030266dd23562f2438bc03
bishal-github <shreesbishal44@gmail.com>
2022-11-24 14:28:00 +0545

    creating footer.jsx, adding content inside the footer and styling them

---
+++ src/components/Foooter.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Foooter = () => {
+  return (
+   
+    <footer>
+          <div>
+            <h1>Bishal</h1>
+            <p>@all right reserved</p>
+        </div>
+        <div>
+          <h5>Follow Us</h5>
+        <div>
+         <a href="https://www.facebook.com/profile.php?id=100068476404120" target={"blank"}>Facebook</a>
+         <a href="https://github.com/bishal-github" target={"blank"}>Github</a>
+         <a href="https://mail.google.com/mail/u/0/#inbox" target={"blank"}>Gmail</a>
+        </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Foooter
